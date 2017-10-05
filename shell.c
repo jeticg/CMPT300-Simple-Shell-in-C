c25638428b24de6c005c240ee12df4611cb9d8a4
Jetic Gu <jetic@me.com>
2017-10-05 09:56:08 -0700

    clean up

--- shell.c
+++ shell.c
@@ -1,5 +1,6 @@
 // Shell starter file
 // You may make any changes to any part of this file.
+#include "shell.h"
 
 #include <stdio.h>
 #include <stdbool.h>
@@ -7,26 +8,60 @@
 #include <string.h>
 #include <unistd.h>
 
-#define COMMAND_LENGTH 1024
-#define NUM_TOKENS (COMMAND_LENGTH / 2 + 1)
+// Main
+int main(int argc, char* argv[]) {
+    char inputBuffer[COMMAND_LENGTH];
+    char *tokens[NUM_TOKENS];
+    while (true) {
+        // Get command
+        // Use write because we need to use read() to work with
+        // signals, and read() is incompatible with printf().
+        write(STDOUT_FILENO, "> ", strlen("> "));
+        _Bool inBackground = false;
+        readCommand(inputBuffer, tokens, &inBackground);
 
+        // DEBUG: Dump out arguments:
+        #ifdef DEBUG
+        for (int i = 0; tokens[i] != NULL; i++) {
+            write(STDOUT_FILENO, "   Token: ", strlen("   Token: "));
+            write(STDOUT_FILENO, tokens[i], strlen(tokens[i]));
+            write(STDOUT_FILENO, "\n", strlen("\n"));
+        }
+        #endif
+        if (strcmp(tokens[0], "exit") == 0 || strcmp(tokens[0], "quit") == 0) {
+            return 0;
+        }
+        if (inBackground) {
+            write(STDOUT_FILENO, "Run in background.",
+                  strlen("Run in background."));
+        }
 
-/**
- * Command Input and Processing
- */
+        /**
+         * Steps For Basic Shell:
+         * 1. Fork a child process
+         * 2. Child process invokes execvp() using results in token array.
+         * 3. If inBackground is false, parent waits for
+         *    child to finish. Otherwise, parent loops back to
+         *    readCommand() again immediately.
+         */
 
-/*
- * Tokenise the string in 'buff' into 'tokens'.
- * buff: Character array containing string to Tokenise.
- *       Will be modified: all whitespace replaced with '\0'
- * tokens: array of pointers of size at least COMMAND_LENGTH/2 + 1.
- *       Will be modified so tokens[i] points to the i'th token
- *       in the string buff. All returned tokens will be non-empty.
- *       NOTE: pointers in tokens[] will all point into buff!
- *       Ends with a null pointer.
- * returns: number of tokens.
- */
+    }
+    return 0;
+}
+
+// Functions
 int tokeniseCommand(char *buff, char *tokens[]) {
+    /*
+        Tokenise the string in 'buff' into 'tokens'.
+        buff: Character array containing string to Tokenise.
+              Will be modified: all whitespace replaced with '\0'
+        tokens: array of pointers of size at least COMMAND_LENGTH/2 + 1.
+                Will be modified so tokens[i] points to the i'th token
+                in the string buff. All returned tokens will be non-empty.
+                NOTE: pointers in tokens[] will all point into buff!
+                      Ends with a null pointer.
+        returns: number of tokens.
+    */
     int tokenCount = 0;
     _Bool inToken = false;
     int numChars = strnlen(buff, COMMAND_LENGTH);
@@ -53,18 +88,20 @@ int tokeniseCommand(char *buff, char *tokens[]) {
     return tokenCount;
 }
 
-/**
- * Read a command from the keyboard into the buffer 'buff' and Tokenise it
- * such that 'tokens[i]' points into 'buff' to the i'th token in the command.
- * buff: Buffer allocated by the calling code. Must be at least
- *       COMMAND_LENGTH bytes long.
- * tokens[]: Array of character pointers which point into 'buff'. Must be at
- *       least NUM_TOKENS long. Will strip out up to one final '&' token.
- *       tokens will be NULL terminated (a NULL pointer indicates end of tokens).
- * inBackground: pointer to a boolean variable. Set to true if user entered
- *       an & as their last token; otherwise set to false.
- */
 void readCommand(char *buff, char *tokens[], _Bool *inBackground) {
+    /*
+        Read a command from the keyboard into the buffer 'buff' and Tokenise it
+        such that 'tokens[i]' points into 'buff' to the i'th token in the
+        command.
+        buff: Buffer allocated by the calling code. Must be at least
+              COMMAND_LENGTH bytes long.
+        tokens[]: Array of character pointers which point into 'buff'. Must be
+                  at least NUM_TOKENS long. Will strip out up to one final '&'
+                  token. Tokens will be NULL terminated (a NULL pointer
+                  indicates end of tokens).
+        inBackground: pointer to a boolean variable. Set to true if user
+                      entered an & as their last token; otherwise set to false.
+    */
     *inBackground = false;
 
     // Read input
@@ -94,50 +131,18 @@ void readCommand(char *buff, char *tokens[], _Bool *inBackground) {
     }
 }
 
-void execSingleCommand(char *tokens[]) {
-
+void execSingleCommand(char *tokens[], EXECUTION_CODE executionCode) {
+    /*
+        execCommand
+        This function will execute a single command.
+    */
 }
 
 void execCommand(char *tokens[]) {
+    /*
+        execCommand
+        This function will process the tokens into different commmands and execute
+        them by calling execSingleCommand.
+    */
 
-}
-/**
- * Main and Execute Commands
- */
-int main(int argc, char* argv[]) {
-    char inputBuffer[COMMAND_LENGTH];
-    char *tokens[NUM_TOKENS];
-    while (true) {
-        // Get command
-        // Use write because we need to use read() to work with
-        // signals, and read() is incompatible with printf().
-        write(STDOUT_FILENO, "> ", strlen("> "));
-        _Bool inBackground = false;
-        readCommand(inputBuffer, tokens, &inBackground);
-
-        // DEBUG: Dump out arguments:
-        for (int i = 0; tokens[i] != NULL; i++) {
-
-            write(STDOUT_FILENO, "   Token: ", strlen("   Token: "));
-            write(STDOUT_FILENO, tokens[i], strlen(tokens[i]));
-            write(STDOUT_FILENO, "\n", strlen("\n"));
-            if (strcmp(tokens[i], "exit") == 0) {
-                return 0;
-            }
-        }
-        if (inBackground) {
-            write(STDOUT_FILENO, "Run in background.", strlen("Run in background."));
-        }
-
-        /**
-         * Steps For Basic Shell:
-         * 1. Fork a child process
-         * 2. Child process invokes execvp() using results in token array.
-         * 3. If inBackground is false, parent waits for
-         *    child to finish. Otherwise, parent loops back to
-         *    readCommand() again immediately.
-         */
-
-    }
-    return 0;
 }
---
+++ shell.h
@@ -0,0 +1,20 @@
+#ifndef JETIC_SHELL
+
+// Constants
+#define COMMAND_LENGTH 1024
+#define NUM_TOKENS (COMMAND_LENGTH / 2 + 1)
+
+// Command execution modes.
+#define EXECUTION_CODE int
+#define DIRECT_EXECUTION 0001
+#define BACKGROUND_EXECUTION 0002
+#define BACKGROUND_NO_TRACE_EXECUTION 0003
+
+#define JETIC_SHELL
+#endif
+
+// Functions
+int tokeniseCommand(char *buff, char *tokens[]);
+void readCommand(char *buff, char *tokens[], _Bool *inBackground);
+void execSingleCommand(char *tokens[], EXECUTION_CODE executionCode);
+void execCommand(char *tokens[]);
