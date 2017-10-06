b45b4844deb2b44b0bbd01965e038c5296d2bf3c
Jetic Gu <jetic@me.com>
2017-10-06 10:19:14 -0700

    More error code

--- errorExplain.c
+++ errorExplain.c
@@ -21,61 +21,238 @@
 
 
 char* explainError(int errorCode) {
-    if (errorCode == E2BIG)
-        return "The total number of bytes in the environment (envp) and argument list (argv) is too large";
+    switch(errorCode) {
+        case EADDRINUSE:
+            return "Address already in use.";
 
-    if (errorCode == EACCES)
-        return "Access denied";
+        case EADDRNOTAVAIL:
+            return "Address not available.";
 
-    if (errorCode == EFAULT)
-        return "filename points outside your accessible address space";
+        case EAFNOSUPPORT:
+            return "Address family not supported.";
 
-    if (errorCode == EINVAL)
-        return "An ELF executable had more than one PT_INTERP segment (i.e., tried to name more than one interpreter)";
+        case EAGAIN:
+            return "Resource temporarily unavailable.";
 
-    if (errorCode == EIO)
-        return "An I/O error occurred";
+        case EALREADY:
+            return "Connection already in progress.";
 
-    if (errorCode == EISDIR)
-        return "An ELF interpreter was a directory";
+        case EBADF:
+            return "Bad file descriptor.";
 
-    // This error code appears to be undeclared for some reason
-    // if (errorCode == ELIBBAD)
-    //    return "An ELF interpreter was not in a recognized format";
+        case EBADMSG:
+            return "Bad message.";
 
-    if (errorCode == ELOOP)
-        return "Too many symbolic links were encountered in resolving filename or the name of a script or ELF interpreter";
+        case EBUSY:
+            return "Device or resource busy.";
 
-    if (errorCode == EMFILE)
-        return "The process has the maximum number of files open";
+        case ECANCELED:
+            return "Operation canceled.";
 
-    if (errorCode == ENAMETOOLONG)
-        return "filename is too long";
+        case ECHILD:
+            return "No child processes.";
 
-    if (errorCode == ENFILE)
-        return "The system limit on the total number of open files has been reached";
+        case ECONNABORTED:
+            return "Connection aborted.";
 
-    if (errorCode == ENOENT)
-        return "The file filename or a script or ELF interpreter does not exist, or a shared library needed for file or interpreter cannot be found";
+        case ECONNREFUSED:
+            return "Connection refused.";
 
-    if (errorCode == ENOEXEC)
-        return "An executable is not in a recognized format, is for the wrong architecture, or has some other format error that means it cannot be executed";
+        case ECONNRESET:
+            return "Connection reset.";
 
-    if (errorCode == ENOMEM)
-        return "Insufficient kernel memory was available";
+        case EDEADLK:
+            return "Resource deadlock avoided.";
 
-    if (errorCode == ENOTDIR)
-        return "A component of the path prefix of filename or a script or ELF interpreter is not a directory";
+        case EDESTADDRREQ:
+            return "Destination address required.";
 
-    if (errorCode == EPERM)
-        return "The file system is mounted nosuid or the process is being traced, the user is not the superuser, and the file has the set-user-ID or set-group-ID bit set";
+        case EDOM:
+            return "Mathematics argument out of domain of function.";
 
-    if (errorCode == ETXTBSY)
-        return "Executable was open for writing by one or more processes";
+        case EDQUOT:
+            return "Disk quota exceeded.";
 
-    if (errorCode == EBADF)
-        return "fd is not a valid file descriptor.";
-    return "Unknown error code";
+        case EEXIST:
+            return "File exists.";
+
+        case EFAULT:
+            return "Bad address.";
+
+        case EFBIG:
+            return "File too large.";
+
+        case EHOSTUNREACH:
+            return "Host is unreachable.";
+
+        case EIDRM:
+            return "Identifier removed.";
+
+        case EILSEQ:
+            return "Invalid or incomplete multibyte or wide character.";
+
+        case EINPROGRESS:
+            return "Operation in progress.";
+
+        case EINTR:
+            return "Interrupted function call.";
+
+        case EINVAL:
+            return "Invalid argument.";
+
+        case EIO:
+            return "Input/output error.";
+
+        case EISCONN:
+            return "Socket is connected.";
+
+        case EISDIR:
+            return "Is a directory.";
+
+        case ELOOP:
+            return "Too many levels of symbolic links.";
+
+        case EMFILE:
+            return "Too many open files.";
+
+        case EMLINK:
+            return "Too many links.";
+
+        case EMSGSIZE:
+            return "Message too long.";
+
+        case EMULTIHOP:
+            return "Multihop attempted.";
+
+        case ENAMETOOLONG:
+            return "Filename too long.";
+
+        case ENETDOWN:
+            return "Network is down.";
+
+        case ENETRESET:
+            return "Connection aborted by network.";
+
+        case ENETUNREACH:
+            return "Network unreachable.";
+
+        case ENFILE:
+            return "Too many open files in system.";
+
+        case ENOBUFS:
+            return "No buffer space available.";
+
+        case ENODATA:
+            return "No message is available on the STREAM head read queue.";
+
+        case ENODEV:
+            return "No such device.";
+
+        case ENOENT:
+            return "No such file or directory.";
+
+        case ENOEXEC:
+            return "Exec format error.";
+
+        case ENOLCK:
+            return "No locks available.";
+
+        case ENOLINK:
+            return "Link has been severed.";
+
+        case ENOMEM:
+            return "Not enough space.";
+
+        case ENOMSG:
+            return "No message of the desired type.";
+
+        case ENOPROTOOPT:
+            return "Protocol not available.";
+
+        case ENOSPC:
+            return "No space left on device.";
+
+        case ENOSR:
+            return "No STREAM resources.";
+
+        case ENOSTR:
+            return "Not a STREAM.";
+
+        case ENOSYS:
+            return "Function not implemented.";
+
+        case ENOTCONN:
+            return "The socket is not connected.";
+
+        case ENOTDIR:
+            return "Not a directory.";
+
+        case ENOTEMPTY:
+            return "Directory not empty.";
+
+        case ENOTSOCK:
+            return "Not a socket.";
+
+        case ENOTSUP:
+            return "Operation not supported.";
+
+        case ENOTTY:
+            return "Inappropriate I/O control operation.";
+
+        case ENXIO:
+            return "No such device or address.";
+
+        case EOPNOTSUPP:
+            return "Operation not supported on socket.";
+
+        case EOVERFLOW:
+            return "Value too large to be stored in data type.";
+
+        case EPERM:
+            return "Operation not permitted.";
+
+        case EPIPE:
+            return "Broken pipe.";
+
+        case EPROTO:
+            return "Protocol error.";
+
+        case EPROTONOSUPPORT:
+            return "Protocol not supported.";
+
+        case EPROTOTYPE:
+            return "Protocol wrong type for socket.";
+
+        case ERANGE:
+            return "Result too large.";
+
+        case EROFS:
+            return "Read-only filesystem.";
+
+        case ESPIPE:
+            return "Invalid seek.";
+
+        case ESRCH:
+            return "No such process.";
+
+        case ESTALE:
+            return "Stale file handle.";
+
+        case ETIME:
+            return "Timer expired.";
+
+        case ETIMEDOUT:
+            return "Connection timed out.";
+
+        case ETXTBSY:
+            return "Text file busy.";
+
+        case EXDEV:
+            return "Improper link.";
+
+        default:
+            return "Unknown error code";
+    }
 }
 
 void printErrorMsg(int errorCode) {
